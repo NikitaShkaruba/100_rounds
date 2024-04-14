cfce7b900e4774e34329575a055379c76e113b7c
Nikita Shkaruba <sh.sigmaone@gmail.com>
2024-04-14 22:35:53 +0300

    Add runDirection to EnemyRunner

--- Assets/Scripts/EnemyRunner.cs
+++ Assets/Scripts/EnemyRunner.cs
@@ -4,14 +4,40 @@ using UnityEngine;
 
 public class EnemyRunner : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D rigidbody;
+    
+    public enum Direction
+    {
+        Up,
+        Right,
+        Down,
+        Left,
+    };
 
+    [SerializeField] private Rigidbody2D rigidbody;
+    [SerializeField] private Direction runDirection;
     // Update is called once per frame
     void FixedUpdate()
     {
         const float speed = 4f;
+        float verticalVelocity = 0;
+        float horizontalVelocity = 0;
+        
+        switch (runDirection) {
+            case Direction.Up:
+                verticalVelocity = speed;
+                break;
+            case Direction.Right:
+                horizontalVelocity = speed;
+                break;
+            case Direction.Down:
+                verticalVelocity = -speed;
+                break;
+            case Direction.Left:
+                horizontalVelocity = -speed;
+                break;
+        }
 
-        rigidbody.velocity = new Vector2(1 * speed, 0);
+        rigidbody.velocity = new Vector2(horizontalVelocity, verticalVelocity);
     }
     
     // Disable this script when the GameObject moves out of the camera's view
